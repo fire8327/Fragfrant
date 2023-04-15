8d8e41ff7345cab8d55f974ff17147da9ec599df
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-04-15 16:00:45 +0000

    create dropdown

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -3,3 +3,14 @@ $("#filter").click(() => {
   $("#filters").slideToggle(500);
   $("#filter").toggleClass("border-b");
 });
+
+/* dropdown */
+$(".dropdown").each((i,el) => { 
+  let list =  $(el).find('.dropdown-list');
+  let dropdown = $(el).find('.dropdown-toggler');
+  dropdown.click(() => {
+    $(el).toggleClass("bg-[#A7A1E9]");
+    $(el).toggleClass("text-white");
+    list.slideToggle(500);
+  });
+});
